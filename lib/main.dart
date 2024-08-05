4603579fd807a31cab04af1cfb741ab6ab71f40e
Mehmet Akif Türker <mehmetakifturker@gmail.com>
2024-08-05 10:06:16 +0300

    register ve login view

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'firebase_options.dart';
+import 'package:learningdart/firebase_options.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  var appName = "Flutter Demo";
+  runApp(
+    MaterialApp(
+      title: appName,
+      // Create a custom theme with Material Design 3 and a color scheme
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
+      // Set the home screen of the app
       home: const HomePage(),
-    );
-  }
+    ),
+  );
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text("Home"),
         backgroundColor: Colors.blue,
-        title: const Text(
-          "REGISTER",
-          style: TextStyle(color: Colors.white),
-        ),
       ),
       body: FutureBuilder(
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(hintText: "Email"),
-                    controller: _email,
-                    enableSuggestions: true,
-                    autocorrect: true,
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(hintText: "Password"),
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      try {
-                        final userCredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        print(userCredential);
-                      } catch (e) {
-                        print(e);
-                      }
-                    },
-                    child: const Text("Register button"),
-                  ),
-                ],
-              );
-            default:
-              return const Text("Loading");
-          }
-        },
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              final emailVerified = user?.emailVerified ?? false;
+              if (emailVerified) {
+                print("Verified User");
+              } else
+                print("You need to verify email");
+              return const Text("DONE");
+            default:
+              return const Text("Loading....");
+          }
+        },
       ),
     );
   }
