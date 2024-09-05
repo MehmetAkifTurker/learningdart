86e5bd39cb369933cd85f895c93e200c4d2f0224
Mehmet Akif Türker <mehmetakifturker@gmail.com>
2024-09-05 11:53:55 +0300

    servicesAdded

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -5,6 +5,8 @@ import 'package:learningdart/constants/routes.dart';
 import 'package:learningdart/firebase_options.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:learningdart/utilities/show_error_dialog.dart';
+
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
 
@@ -67,20 +69,41 @@ class _RegisterViewState extends State<RegisterView> {
                       final email = _email.text;
                       final password = _password.text;
                       try {
-                        final userCredential = await FirebaseAuth.instance
+                        await FirebaseAuth.instance
                             .createUserWithEmailAndPassword(
                           email: email,
                           password: password,
                         );
-                        devtools.log(userCredential.toString());
+                        final user = FirebaseAuth.instance.currentUser;
+                        await user?.sendEmailVerification();
+                        Navigator.of(context).pushNamed(verifyEmailRoute);
                       } on FirebaseAuthException catch (e) {
                         if (e.code == "weak-password") {
-                          devtools.log("weak-password");
+                          await showErrorDialog(
+                            context,
+                            "Weak password",
+                          );
                         } else if (e.code == "email-already-in-use") {
-                          devtools.log("email is already in use");
+                          await showErrorDialog(
+                            context,
+                            "email is already in use",
+                          );
                         } else if (e.code == "invalid-email") {
-                          devtools.log("invalid email");
+                          await showErrorDialog(
+                            context,
+                            "invalid email",
+                          );
+                        } else {
+                          await showErrorDialog(
+                            context,
+                            "Something went wrong: ${e.message}",
+                          );
                         }
+                      } catch (e) {
+                        await showErrorDialog(
+                          context,
+                          e.toString(),
+                        );
                       }
                     },
                     child: const Text("Register"),
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -56,7 +56,7 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
           TextButton(
             onPressed: () async {
               final user = FirebaseAuth.instance.currentUser;
-              print(user);
+
               await user?.sendEmailVerification();
             },
             child: const Text("Send email verification"),
